cca5be3790e0dc8a45d6f6635160e3f33994bb68
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-20 22:00:09 +0530

    add login page changes

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 
 function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  function handleSubmit() {
-    setEmail();
-    console.log("login");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
   }
   return (
     <div>
       <h1>Login From</h1>
-      <input name="email" type="email" placeholder="email..." value={email} />
+      <input
+        name="email"
+        type="email"
+        placeholder="email..."
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <input
         name="password"
         type="password"
         placeholder="password..."
         value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <button onClick={handleSubmit}>submit</button>
     </div>
