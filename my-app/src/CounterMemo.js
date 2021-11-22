59abd4fb3199baf4446d2134e1b32356d564e2f2
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-22 17:08:44 +0530

    add useMemo hook

--- my-app/src/CounterMemo.js
+++ my-app/src/CounterMemo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 function CounterMemo() {
   const [counterOne, setCounterOne] = useState(0);
@@ -6,14 +6,22 @@ function CounterMemo() {
 
   const incrementOne = () => {
     setCounterOne(counterOne + 1);
+    console.log(counterOne);
   };
+
   const incrementTwo = () => {
     setCounterTwo(counterTwo + 1);
   };
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 20000000000) i++;
+    return counterOne % 2 === 0;
+  }, [counterOne]);
   return (
     <div>
       <div>
         <button onClick={incrementOne}>counter One: {counterOne}</button>
+        <span>{isEven ? " Even" : " Odd"}</span>
       </div>
       <div>
         <button onClick={incrementTwo}>Counter Two: {counterTwo}</button>
