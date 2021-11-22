17ab932702970e9f4d6949afef5bbf62cdfea601
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-22 15:18:09 +0530

    add parentcomponent file

--- my-app/src/Component/ParentComponent.js
+++ my-app/src/Component/ParentComponent.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Count from "./Count";
+import Title from "./Title";
+import Button from "./Button";
 
 function ParentComponent() {
+  const [age, setAge] = useState();
+  const [salary, setSalary] = useState(10000);
+  const incrementAge = () => {
+    setAge(age + 1);
+  };
+  const incrementSalary = () => {
+    setSalary(salary + 5000);
+  };
   return (
     <div>
-      <h1>parent</h1>
+      <Title />
+      <Count text="Age" count={age} />
+      <Button handleClick={incrementAge}>Increment Age</Button>
+      <Count text="Salary" count={salary} />
+      <Button handleClick={incrementSalary}>Increment Salary</Button>
     </div>
   );
 }
